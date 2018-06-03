323687b9935fb0b135ef2b318359340c08d45e3e
Daniel Caldwell <daniel@danielcaldwell.com>
2018-06-03 11:07:35 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+IMAGE_NAME=danielcaldwell/nettools
+IMAGE_VERSION=1.0
+
+if [ "$(docker ps -a | grep $IMAGE_NAME:$IMAGE_VERSION)" ]; then
+   echo "Containers exist using the image, unable to build. "
+   echo "Check containers using:"
+   echo "  docker ps -a | grep "$IMAGE_NAME:$IMAGE_VERSION
+   echo "Remove containers using:"
+   echo "  docker ps -a | grep  $IMAGE_NAME:$IMAGE_VERSION| awk '{print \$1}' | xargs docker rm"
+   exit 1
+fi
+
+if [ "$(docker images | grep $IMAGE_NAME:$IMAGE_VERSION)" ]; then
+   docker rmi $IMAGE_NAME:$IMAGE_VERSION
+fi
+
+docker build --force-rm --tag $IMAGE_NAME:$IMAGE_VERSION .
